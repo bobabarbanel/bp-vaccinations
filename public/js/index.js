c5d6ffe09a6c58a2bb5b544f29b5aace936c1a43
bobabarbanel <bob.abarbanel@gmail.com>
2022-11-19 16:11:27 -0800

    documentation

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,15 @@ $(function() {
 	if (month < 10) month = "0" + month;
 	let day = today.getDate();
 	if (day < 10) day = "0" + day;
-	const button = $("button");
+	const button = $("button"); // submit button
 
-	$("#datepicker").datepicker("setDate", null);
+	$("#datepicker").datepicker("setDate", null); // start with no date chosen
 
+	// formatted yyyy-mm-dd date
 	$("#selectedDate").text(`${today.getFullYear()}-${month}-${day}`);
 
+
+	// future dates OK
 	$("#datepicker").datepicker({
 		dateFormat: "yy-mm-dd",
 		onSelect: function(selectedDate) {
@@ -22,34 +25,39 @@ $(function() {
 		}
 	});
 
-	$("li > input").on("change", handleSite);
+	$("li > input").on("change", handleSite); // enable submit button when date chosen
+	// take click oin words same as radio button
 	$("li > span").on("click", e => {
 		$(e.target).parent().find("input").trigger("click");
 	});
-	button.on("click", launch);
-	function handleSite() {
+	button.on("click", launch); // open app when submit button pushed
+
+	function handleSite() { // highlight and enable submit button
 		$("ul li").css("background", "white");
 		$(this).parent().css("background", "orange");
 		button.prop("disabled", false);
 	}
 	function launch() {
+		// gather parameters for URL
 		const startDate = $("#selectedDate").text().trim();
 		const location = $(".site input:checked").parent().text().trim();
 		const locationId = $(".site input:checked").val();
 
 		$("container").hide();
-		const app = $("#app").text();
+		const app = $("#app").text(); // grab 'allvac' or 'byvac' name
 		$("loading").show();
 		fade_in($("loading"));
+		// open data app in this window
 		window.open(
 			`/appts/${startDate}/${encodeURI(location)}/${locationId}/${app}`,
 			"_self"
 		);
 	}
+	// Show Loading with fade-in up to 3 times
 	async function fade_in(loadText) {
 		let opacity = 0;
 		loadText.css("background-color", `rgba(0,0,255,${opacity})`);
-		let three = 3;
+		let three = 3; // cycle to full opacity three times max
 		const interval = setInterval(() => {
 			opacity += 0.1;
 			if (opacity > 1) {
